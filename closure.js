1d4cbf022326c3ae7168d310e1a0cb24a2a3a5fc
GaniyuSodiq <82044466+GaniyuSodiq@users.noreply.github.com>
2025-07-02 17:46:21 +0100

    intro to closure

--- closure.js
+++ closure.js
@@ -1,3 +1,19 @@
 // A closure is the ability to access a parent level scope 
 // from a child scope, even after the parent function has been terminated.
+console.log("IT IS WORKING")
 
+function outer() {
+    const outerVar = 'I am outer var'
+    function inner() {
+        const innerVar = 'I am inner var'
+        console.log(innerVar);
+        console.log(outerVar);
+    }
+//     inner()
+    return inner
+ }
+
+// outer()
+
+const innerFn = outer()
+innerFn()
