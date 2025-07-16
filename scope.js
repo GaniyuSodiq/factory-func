6f57472abee6486efd5ede1691eb07b56aecef03
GaniyuSodiq <82044466+GaniyuSodiq@users.noreply.github.com>
2025-07-16 08:20:53 +0100

    modify the scope comments

--- scope.js
+++ scope.js
@@ -19,9 +19,13 @@ function go() {
   console.log(age);
   console.log(hair);
 }
-// In the example above 👆, if we wanted to access the age = 100; from the go() function, there is no way to do that because the variable has been shadowed (meaning it has been over-written).
-// A tip is that if you ever have a variable inside of a function that is very similar to a variable outside of a function, name the variable inside of the function more specifically so you have access to both.
-// In this case, inside of the function you could declare a variable like const myAge = 200; instead of just calling it age.
+// In the example above 👆, if we wanted to access the age = 100; from the go() function, 
+// there is no way to do that because the variable has been shadowed (meaning it has been over-written).
+// A tip is that if you ever have a variable inside of a function that is very similar to a...
+// variable outside of a function, name the variable inside of the function more specifically... 
+// so you have access to both.
+// In this case, inside of the function you could declare a variable like const myAge = 200; 
+// instead of just calling it age.
 
 for (var i = 0; i < 10; i++) {
    console.log(i);
